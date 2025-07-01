1a7442fbb16d3bca2158612e82d30ce4de05b9cb
Balamurugan-Testpress <balamurugan@testpress.in>
2025-07-01 07:11:28 +0000

    completed chapter 6

--- actions/utils.py
+++ actions/utils.py
@@ -17,7 +17,6 @@ def create_action(user, verb, target=None):
             target_ct=target_ct, target_id=target.id
         )
     if not similar_actions:
-        # no existing actions found
         action = Action(user=user, verb=verb, target=target)
         action.save()
         return True
